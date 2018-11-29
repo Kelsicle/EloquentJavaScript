4fb4310bb789a27ab975af2c1c23aead8f41980b
Kelsicle <tom.a.kelly89@gmail.com>
2018-11-30 06:52:32 +1100

    HigherOrderFunctions Chapter of eloquentJS

--- eloquentJS/RobotProject.js
+++ eloquentJS/RobotProject.js
@@ -50,13 +50,13 @@ class VillageState {
 function runRobot(state, robot, memory) {
     for (let turn = 0; ; turn++) {
         if (state.parcels.length == 0) {
-            console.log('Done in ${turn} turns.');
+            console.log(`Done in ${turn} turns.`);
             break;
         }
         let action = robot(state, memory);
         state = state.move(action.direction);
         memory = action.memory;
-        console.log("Moved to ${action.direction}");
+        console.log(`Moved to ${action.direction}`);
     }
 }
 
@@ -82,4 +82,4 @@ VillageState.random = function (parcelCount = 5) {
     return new VillageState("Post Office", parcels);
 };
 
-runRobot(VillageState.random(), randomRobot());
+runRobot(VillageState.random(), randomRobot);
